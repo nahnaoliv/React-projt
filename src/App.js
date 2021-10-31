07721c38a320e3c1bd7f2304ea7bd6afa2183536
mariana oliveira <91402583+nahnaoliv@users.noreply.github.com>
2021-10-31 02:07:07 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Card from './componentes/card';
+
+const App = () => {
+    return (
+        <>
+        <h1>Trabalhando com Cards - Pratica inicial em React</h1>
+        <Card/>
+        </>
+    )
+}
+
+export default App;
